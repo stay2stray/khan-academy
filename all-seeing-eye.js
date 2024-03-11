e2992260da854b375e63aa640b54c20830a3d995
stay2stray <staystray@yandex.ru>
2024-03-11 18:52:30 +0300

    first commit

---
+++ all-seeing-eye.js
@@ -0,0 +1,27 @@
+background(0, 0, 0);
+fill(104, 0, 232);
+textSize(44);
+text("All-Seeing Eye", 57, 66);
+textSize(22);
+text("Sees all!", 32, 130);
+textSize(10);
+text("Even theese tiny-weeny letters", 19, 142);
+textSize(26);
+text("No unneccessary functions.", 43, 341);
+textSize(23);
+text("Just eye.", 273, 374);
+textSize(29);
+text("Get yours today!", 171, 119);
+
+stroke(42, 0, 79);
+strokeWeight(10);
+ellipse(200, 220, 210, 123);
+ellipse(200, 220, 98, 101);
+stroke(255, 255, 255);
+strokeWeight(3);
+line(200, 176, 200, 263);
+line(157, 220, 242, 220);
+stroke(252, 237, 101);
+strokeWeight(22);
+point(200, 220); 
+
